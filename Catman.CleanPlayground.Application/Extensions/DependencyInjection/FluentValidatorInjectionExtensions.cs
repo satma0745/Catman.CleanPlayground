86aa208421446bdf8f22f18691822bf03d5e02b9
catman0745 <catman0745@gmail.com>
2021-07-05 22:50:57 +0300

    Automatically register application validators

--- Catman.CleanPlayground.Application/Extensions/DependencyInjection/FluentValidatorInjectionExtensions.cs
+++ Catman.CleanPlayground.Application/Extensions/DependencyInjection/FluentValidatorInjectionExtensions.cs
@@ -1,24 +1,12 @@
 namespace Catman.CleanPlayground.Application.Extensions.DependencyInjection
 {
-    using Catman.CleanPlayground.Application.Services.Authentication.Requests;
-    using Catman.CleanPlayground.Application.Services.Users.Requests;
+    using System.Reflection;
     using FluentValidation;
     using Microsoft.Extensions.DependencyInjection;
 
     internal static class FluentValidatorInjectionExtensions
     {
         public static IServiceCollection AddValidators(this IServiceCollection services) =>
-            services
-                .AddUserValidators()
-                .AddAuthenticationValidators();
-
-        private static IServiceCollection AddUserValidators(this IServiceCollection services) =>
-            services
-                .AddScoped<IValidator<RegisterUserRequest>, RegisterUserRequestValidator>()
-                .AddScoped<IValidator<UpdateUserRequest>, UpdateUserRequestValidator>()
-                .AddScoped<IValidator<DeleteUserRequest>, DeleteUserRequestValidator>();
-
-        private static IServiceCollection AddAuthenticationValidators(this IServiceCollection services) =>
-            services.AddScoped<IValidator<AuthenticateUserRequest>, AuthenticateUserRequestValidator>();
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly(), includeInternalTypes: true);
     }
 }
