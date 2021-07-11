010f4f5b5b01c3ef7a22c51b33ba95db528f847c
catman0745 <catman0745@gmail.com>
2021-07-11 11:43:58 +0300

    Disable SwaggerUI Schemas section

--- Catman.CleanPlayground.WebApi/Extensions/DependencyInjection/SwaggerInjectionExtensions.cs
+++ Catman.CleanPlayground.WebApi/Extensions/DependencyInjection/SwaggerInjectionExtensions.cs
@@ -6,30 +6,16 @@ namespace Catman.CleanPlayground.WebApi.Extensions.DependencyInjection
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.OpenApi.Models;
+    using Swashbuckle.AspNetCore.SwaggerGen;
+    using Swashbuckle.AspNetCore.SwaggerUI;
 
     internal static class SwaggerInjectionExtensions
     {
         public static IServiceCollection AddSwagger(this IServiceCollection services, IConfiguration configuration) =>
             services.AddSwaggerGen(options =>
             {
-                var title = configuration.GetApplicationTitle();
-                var version = $"v{configuration.GetApplicationVersion()}";
-                
-                options.SwaggerDoc(version, new OpenApiInfo
-                {
-                    Title = title,
-                    Version = version
-                });
-                
-                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-                {
-                    Description = "Provide authorization token:",
-                    Type = SecuritySchemeType.Http,
-                    BearerFormat = "JWT",
-                    Scheme = "bearer"
-                });
-
-                options.OperationFilter<SwaggerAuthorizationIndicatorFilter>();
+                options.AddOpenApiInfo(configuration);
+                options.AddSecurity();
             });
 
         public static IApplicationBuilder UseSwagger(
@@ -39,16 +25,49 @@ namespace Catman.CleanPlayground.WebApi.Extensions.DependencyInjection
                 .UseSwagger()
                 .UseSwaggerUI(options =>
                 {
-                    var title = configuration.GetApplicationTitle();
-                    var version = $"v{configuration.GetApplicationVersion()}";
-                    
-                    options.SwaggerEndpoint(
-                        url: $"/swagger/{version}/swagger.json",
-                        name: $"{title} {version}");
-
-                    options.RoutePrefix = string.Empty;
-                    
+                    options.AddSwaggerEndpoint(configuration);
                     options.InjectStylesheet("/swagger.css");
+                    options.DisableSchemasSection();
                 });
+
+        private static void AddOpenApiInfo(this SwaggerGenOptions options, IConfiguration configuration)
+        {
+            var title = configuration.GetApplicationTitle();
+            var version = $"v{configuration.GetApplicationVersion()}";
+                
+            options.SwaggerDoc(version, new OpenApiInfo
+            {
+                Title = title,
+                Version = version
+            });
+        }
+
+        private static void AddSecurity(this SwaggerGenOptions options)
+        {
+            options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Description = "Provide authorization token:",
+                Type = SecuritySchemeType.Http,
+                BearerFormat = "JWT",
+                Scheme = "bearer"
+            });
+
+            options.OperationFilter<SwaggerAuthorizationIndicatorFilter>();
+        }
+
+        private static void AddSwaggerEndpoint(this SwaggerUIOptions options, IConfiguration configuration)
+        {
+            var title = configuration.GetApplicationTitle();
+            var version = $"v{configuration.GetApplicationVersion()}";
+                    
+            options.SwaggerEndpoint(
+                url: $"/swagger/{version}/swagger.json",
+                name: $"{title} {version}");
+            
+            options.RoutePrefix = string.Empty;
+        }
+
+        private static void DisableSchemasSection(this SwaggerUIOptions options) =>
+            options.DefaultModelsExpandDepth(-1);
     }
 }
